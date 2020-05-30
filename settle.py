1688aa8e30f0d351dca6443e4f7ee0009f2e19fc
Connor Goggins <cgoggins0@gmail.com>
2020-05-29 21:24:11 -0700

    Added script & sample JSON file

---
+++ settle.py
@@ -0,0 +1,21 @@
+import json
+
+with open("players.json") as json_file:
+    players = json.load(json_file)["players"]
+
+    net = 0.0
+    for i in range(len(players)):
+        net += players[i]["earnings"]
+
+    if net != 0.0:
+        raise Exception('Imbalance detected. Total gains do not equal total losses.')
+
+    for i in range(len(players)):
+        j = 0
+        while players[i]["earnings"] < 0.0:
+            if j != i and players[j]["earnings"] > 0.0:
+                amount = min(abs(players[i]["earnings"]), players[j]["earnings"])
+                players[i]["earnings"] += amount
+                players[j]["earnings"] -= amount
+                print("{} sends {} ${}".format(players[i]["name"], players[j]["name"], amount))
+            j += 1
